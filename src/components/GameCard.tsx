2e6126b2bc9d46c84b02ffda0f01e54aacfe6c4d
Nishant Yadav <inishantyadav24@gmail.com>
2023-04-30 08:01:29 +0530

    Refactor the duplicate styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,10 +10,10 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="1xl">{game.name}</Heading>
         <HStack justifyContent="space-between" marginY={1}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
