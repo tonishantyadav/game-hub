ece4a3d722c717981951bfb2b12a29e2c446db16
Nishant Yadav <inishantyadav24@gmail.com>
2023-05-01 09:23:04 +0530

    Displaying selected genre as bold

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -10,10 +10,11 @@ import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
+  selectedGenre: Genre | null;
   onSelectGenre: (genre: Genre) => void;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { genres, isLoading } = useGenres();
 
   return (
@@ -28,6 +29,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               src={getCroppedImageUrl(genre.image_background)}
             />
             <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
               variant="link"
