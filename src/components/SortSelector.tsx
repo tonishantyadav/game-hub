e4007575dffefe64be081775ba7c3c8288af0507
Nishant Yadav <inishantyadav24@gmail.com>
2023-05-02 13:12:47 +0530

    Displaying sorting selector

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,22 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+const SortSelector = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Order by:
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Relevance</MenuItem>
+        <MenuItem>Date modified</MenuItem>
+        <MenuItem>Name</MenuItem>
+        <MenuItem>Release date</MenuItem>
+        <MenuItem>Popularity</MenuItem>
+        <MenuItem>Average rating</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
