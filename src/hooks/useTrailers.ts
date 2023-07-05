18b807cb49661f4f72a333fb68232fac23b621eb
Nishant Yadav <inishantyadav24@gmail.com>
2023-07-05 22:39:43 +0530

    Refactor- entity interfaces

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -1,6 +1,6 @@
-import { useQueries, useQuery } from "@tanstack/react-query"
-import APIClient from "../services/api-client"
-import { Trailer } from "../entities/Trailer";
+import { useQuery } from "@tanstack/react-query";
+import Trailer from "../entities/Trailer";
+import APIClient from "../services/api-client";
 
 const useTrailers = (gameId: number) => {
     const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
